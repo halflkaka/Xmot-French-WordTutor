566041e2eeaa78a0224111745be11ad419073796
halflkaka <shicanjie@shicanjiedeMacBook-Pro.local>
2017-12-19 09:05:22 +0800

    first commit

---
+++ app/src/main/java/com/example/shicanjie/xmot/WelcomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.shicanjie.xmot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        final View view = View.inflate(this,R.layout.activity_welcome,null);
+        setContentView(view);
+
+        Animation aa = new AlphaAnimation(0.3f,1.0f);
+        aa.setDuration(2000);
+        view.startAnimation(aa);
+        aa.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                redirectTo();
+            }
+        });
+    }
+    private void redirectTo(){
+        Intent intent = new Intent(this, IndexActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
