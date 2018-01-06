762d07ed1774fe38f5488c0114dc68a58bda2dc3
luodihao <ldihao@gmail.com>
2018-01-06 21:54:04 +0800

    Update Log in and Sign in

--- app/src/main/java/com/example/shicanjie/xmot/Activity/UserActivity.java
+++ app/src/main/java/com/example/shicanjie/xmot/Activity/UserActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.shicanjie.xmot.Class.MyTCPSocket;
 import com.example.shicanjie.xmot.R;
@@ -19,6 +20,7 @@ import com.example.shicanjie.xmot.R;
 public class UserActivity extends AppCompatActivity {
 
     Button sign_in;
+    Button register;
     AutoCompleteTextView email;
     EditText password;
     public MyTCPSocket socket_helper;
@@ -47,16 +49,17 @@ public class UserActivity extends AppCompatActivity {
     };
 
 
-    class MyListener implements View.OnClickListener {
+    class MyListener_register implements View.OnClickListener {
         public void onClick(View v){
             try {
-                Connect_Thread connect_Thread = new Connect_Thread();
+                Connect_Thread_register connect_Thread = new Connect_Thread_register();
                 connect_Thread.start();
+                Toast.makeText(UserActivity.this, "Log in!", Toast.LENGTH_SHORT);
 //                TCPclient(email.toString() + "\n" + password.getText());
             } catch (Exception e) {
             }
         }
-        class Connect_Thread extends Thread implements Runnable//继承Thread
+        class Connect_Thread_register extends Thread implements Runnable//继承Thread
         {
             MyTCPSocket clientSocket;
 
@@ -86,6 +89,46 @@ public class UserActivity extends AppCompatActivity {
 
     }
 
+    class MyListener_signin implements View.OnClickListener {
+        public void onClick(View v){
+            try {
+                Toast.makeText(UserActivity.this, "Log in!", Toast.LENGTH_SHORT);
+                Connect_Thread_signin connect_Thread = new Connect_Thread_signin();
+                connect_Thread.start();
+//                TCPclient(email.toString() + "\n" + password.getText());
+            } catch (Exception e) {
+            }
+        }
+        class Connect_Thread_signin extends Thread implements Runnable//继承Thread
+        {
+            MyTCPSocket clientSocket;
+
+
+            public void run()
+            {
+                try {
+                    String modifiedSentence;
+                    socket_helper.getsocket();
+                    modifiedSentence = socket_helper.sendMessage("/signin " + email.getText() + " " + password.getText());
+                    Log.d("UserActivity", modifiedSentence);
+
+
+//                        AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(UserActivity.this);
+//                        Log.d("UserActivity", "7");
+//                        builder.setTitle("提醒");
+//                        builder.setMessage(modifiedSentence);
+//                        builder.setPositiveButton("是", null);
+//                        builder.show();
+//                        Toast.makeText(UserActivity.this, "提醒", Toast.LENGTH_SHORT).show();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,13 +137,15 @@ public class UserActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         sign_in = findViewById(R.id.sign_in_button);
+        register = findViewById(R.id.register_button);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
 
         navigation.getMenu().getItem(2).setChecked(true);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        sign_in.setOnClickListener(new MyListener());
+        sign_in.setOnClickListener(new MyListener_signin());
+        register.setOnClickListener(new MyListener_register());
 
 
     }
